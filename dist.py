c4329babb3eea039748492c44fa4ad093c9ed7c7
santos <33253496+santosuah@users.noreply.github.com>
2020-05-23 15:29:52 +0200

    Eliminado comentarios

--- dist.py
+++ dist.py
@@ -1,23 +1,16 @@
 from math import sqrt
 
-
-puntos = [[6, 10, "base station Leader"], [10, 2, "base station Follower0"], [5, 9, "objective Leader"], [16, 13, "objective Follower0"], [7, 7, "init position Leader"], [7, 2, "init position Follower0"]]
-
-"""
-for p1 in puntos:
-    for p2 in puntos:
-
-        v = sqrt( (p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
-
-        print(p1[2], "-", p2[2], "=", round(v, 2))
-    
-    print()
-"""
+puntos = [
+    [6, 10, "base station Leader"], 
+    [10, 2, "base station Follower0"],
+    [5, 9, "objective Leader"],
+    [16, 13, "objective Follower0"],
+    [7, 7, "init position Leader"],
+    [7, 2, "init position Follower0"]
+]
 
 for i in range(len(puntos)):
     for j in range(i, len(puntos)):
         v = sqrt( (puntos[j][0] - puntos[i][0])**2 + (puntos[j][1] - puntos[i][1])**2)
-
         print(puntos[i][2], "-", puntos[j][2], "=", round(v, 2))
-    
     print()
